dfb7d6f127f8e7ac3656d802733cfcd5eee7e8b1
Melike Karakus <melikekarakus20@gmail.com>
2020-04-23 21:35:12 +0300

    Initial

---
+++ Bussines/Concrete/BookManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Bussines.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+namespace Bussines.Concrete
+{
+	public class BookManager : IBookService
+	{
+		private IBookDal _bookDal;
+
+		public BookManager(IBookDal bookDal)
+		{
+			_bookDal = bookDal;
+		}
+
+		public void Add(Book book)
+		{
+			//throw new NotImplementedException();
+			//EfBookDal efBookDal = new EfBookDal();
+			_bookDal.Add(book);
+			if (_bookDal.Get(b=>b.Name == book.Name && b.AuthorId==book.AuthorId ) == null)
+			{
+				_bookDal.Add(book);
+			}
+			throw new Exception("Bu kitap adı zaten var");
+		}
+
+		public void Delete(Book book)
+		{
+			//throw new NotImplementedException();
+			_bookDal.Delete(book);
+		}
+
+		public List<Book> GetAll()
+		//{
+			//throw new NotImplementedException();
+		return public Book GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Book> GetByAuthorId(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Book book)
+		{
+			throw new NotImplementedException();
+		}
+
+		_bookDal.GetList().ToList();
+		}
+
+		public List<Book> GetByAuthorId(int id)
+		{
+		//throw new NotImplementedException();
+		return _bookDal.GetList(b => b.AuthorId == id).ToList();
+		}
+
+		public Book GetById(int id)
+		//{
+		//	throw new NotImplementedException();
+		return _bookDal.Get(b => b.Id == id);
+		}
+
+		public void Update(Book book)
+		{
+	_bookDal.Update(book);
+	
+
+			//throw new NotImplementedException();
+		}
+	}
+
+}
---
+++ Entities/Concrete/Book.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+	public class Book:IEntity
+	{
+		public int Id { get; set; }
+		public int AuthorId { get; set; }
+		public string Name { get; set; }
+		public int Price { get; set; }
+		public decimal MyProperty { get; set; }
+	}
+}
